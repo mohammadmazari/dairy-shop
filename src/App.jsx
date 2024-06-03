63f855ec7e18872f9aff45fa9c11db2384746d54
mohammadmazari <mazari.js@gmail.com>
2024-06-03 20:19:02 +0330

    start my project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-blue-500 font-vazir font-bold w-full p-2 rounded text-white text-[40px]'>دیری شاپ </h1>
+    </div>
+  )
+}
+
+export default App
